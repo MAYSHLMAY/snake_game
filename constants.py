00586a289d6d6c8147d2631355cdf61d560977eb
Mikiyas dawit <educationforlife424@gmail.com>
2025-08-19 16:12:22 +0300

    feat: add snake head color

--- constants.py
+++ constants.py
@@ -9,7 +9,7 @@ GRID_HEIGHT = SCREEN_HEIGHT // SNAKE_SIZE
 
 ''' SNAKE SPEED '''
 
-FPS = 10  
+FPS = 1
 
 
 ''' SNAKE COLOR, FOOD, ENVIRONMENT '''
@@ -18,6 +18,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+DARK_GREEN = (0, 200, 0)
 BLUE = (0, 0, 255)
 
 ''' DIRECTIONS '''

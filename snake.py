b345631206623357eff4ff87079b9049ace389f0
Mikiyas dawit <educationforlife424@gmail.com>
2025-08-19 23:13:04 +0300

    feat(snake): add movement for the snake in different directions

--- snake.py
+++ snake.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
-from itertools import islice
+# from itertools import islice
 import deque
-from constants import SNAKE_SIZE, BLACK, RED, GREEN, DARK_GREEN, SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import DIRECTIONS, SNAKE_SIZE, BLACK, RED, GREEN, DARK_GREEN, SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 
@@ -16,6 +16,7 @@ class Snake:
     def __init__(self, controls, start_pos):
 
         """Initialize snake with one segment at (x, y)."""
+
         self.segments = deque([start_pos]) 
         self.direction = "RIGHT"
         self.controls = controls
@@ -23,4 +24,21 @@ class Snake:
         self.alive = True 
 
     def move(self):
-        pass
+
+        """Move snake to the current direction"""
+
+        head_x, head_y = self.segments[0]
+        dx, dy = DIRECTIONS[self.direction]
+        head_x += dx
+        head_y += dy
+
+        # adding new head for the snake
+
+        self.segments.appendleft((head_x, head_y))
+
+        # remove tail unless growing
+
+        if not self.grow_pending:
+            self.segments.pop()
+        else:
+            self.grow_pending = False
